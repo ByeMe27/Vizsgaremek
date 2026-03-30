548b1666c8ff29c94dfdf575507b2411435d7850
ByeMe27 <tompebarnabas@gmail.com>
2026-03-30 15:47:38 +0200

    feedback oldalhoz fajlok

---
+++ feedback/feedback.js
@@ -0,0 +1,70 @@
+//////////////////SESSION
+
+const userBtnEl = document.getElementById("user-login");
+const userPopup = document.getElementById("user-popup");
+const popupUsername = document.getElementById("popup-username");
+const logoutBtn = document.getElementById("logout-btn");
+const profileBtn = document.getElementById("profile");
+
+userBtnEl.addEventListener("click", (e) => {
+  e.stopPropagation();
+  userPopup.style.display =
+    userPopup.style.display === "block" ? "none" : "block";
+});
+
+document.addEventListener("click", (e) => {
+  if (!userPopup.contains(e.target) && e.target !== userBtnEl) {
+    userPopup.style.display = "none";
+  }
+});
+
+logoutBtn.addEventListener("click", async () => {
+  try {
+    const res = await fetch("../login/login.php/logout", {
+      credentials: "include",
+    });
+
+    if (!res.ok) throw new Error("Kijelentkezés sikertelen");
+
+    window.location.href = "../login/login.html";
+  } catch (err) {
+    showGlobalAlert(err.message, "danger");
+  }
+});
+
+profileBtn.addEventListener("click", async () => {
+  try {
+    window.location.href = "profile/profile.html";
+  } catch (err) {
+    showGlobalAlert(err.message, "danger");
+  }
+});
+
+async function userBtn() {
+  const res = await fetch("../login/login.php/me");
+  const data = await res.json();
+  if (!res.ok) throw new Error(data.Hiba || "Hiba történt!");
+  console.log(data);
+  userBtnEl.innerHTML =
+    data.name.split(" ")[0].charAt(0).toString().toUpperCase() +
+    data.name.split(" ")[1].charAt(0).toString().toUpperCase();
+}
+
+async function userName() {
+  const res = await fetch("../login/login.php/me");
+  const data = await res.json();
+  if (!res.ok) throw new Error(data.Hiba || "Hiba történt!");
+  popupUsername.innerHTML += `Helló, ${data.name}!`;
+}
+
+document.querySelectorAll(".category-btn").forEach((btn) => {
+  btn.addEventListener("click", kategoriaValasztas);
+});
+window.addEventListener("load", minden);
+document.getElementById("search-btn").addEventListener("click", szures);
+document.getElementById("place-order").addEventListener("click", rendeles);
+
+window.addEventListener("DOMContentLoaded", async () => {
+  await userBtn();
+  await userName();
+});
